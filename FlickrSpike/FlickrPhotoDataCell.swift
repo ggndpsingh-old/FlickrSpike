e06ce8389060d4f7d3d9983260bdc9551984cd8c
ggndpsingh <gagan.deep@me.com>
2016-07-18 12:45:28 +0530

    Photo Data Cell Updated

--- FlickrSpike/FlickrPhotoDataCell.swift
+++ FlickrSpike/FlickrPhotoDataCell.swift
@@ -17,6 +17,7 @@ class FlickrPhotoDataCell: BaseTableCell {
         }
     }
     
+    
     //MARK: Views
     var tagsLabel: UILabel = {
         let label = UILabel(frame: CGRect.zero)
@@ -38,23 +39,33 @@ class FlickrPhotoDataCell: BaseTableCell {
         return label
     }()
     
+    
+    
+    //MARK: Setup Labels
     func setupLabels() {
         
-        tagsLabel.text  = ""
-        viewsLabel.text = ""
+        //Remove labels before each reuse of cell
+        viewsLabel.removeFromSuperview()
+        tagsLabel.removeFromSuperview()
+        timeLabel.removeFromSuperview()
         
+        
+        //Setup Labels
         if let views = flickrPhoto?.views {
-            viewsLabel = createViewsLabel(with: views)
+            viewsLabel = createLabel(withAddedBoldString: "Views:", toString: views, withFontSize: 13)
         }
         
         if let tags = flickrPhoto?.tags {
-            tagsLabel = createTagsLabel(with: tags)
+            let separated = tags.components(separatedBy: .whitespaces).joined(separator: ", ")
+            tagsLabel = createLabel(withAddedBoldString: "Tags:", toString: separated, withFontSize: 13)
         }
         
         if let date = flickrPhoto?.published {
             timeLabel.text = date.longFormat()
         }
         
+        
+        //Add labels to view with constraints
         addSubview(viewsLabel)
         viewsLabel.topAnchor.constraint     (equalTo: topAnchor, constant: 16)              .isActive = true
         viewsLabel.leftAnchor.constraint    (equalTo: leftAnchor, constant: 16)             .isActive = true
@@ -75,46 +86,21 @@ class FlickrPhotoDataCell: BaseTableCell {
     }
     
     
-    func createTagsLabel(with tags: String) -> UILabel {
+    
+    //MARK: Method to create String with Bold Initial string
+    func createLabel(withAddedBoldString boldString: String, toString string: String, withFontSize fontSize: CGFloat) -> UILabel {
         
-        let joined = tags.components(separatedBy: .whitespaces).joined(separator: ", ")
-        
-        let boldAttribute = [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 13)]
-        
-        let attributedString = NSMutableAttributedString(string: "Tags: \(joined)")
-        let range = NSRange(location: 0, length: 5)
-        
+        let boldAttribute = [NSFontAttributeName: UIFont.boldSystemFont(ofSize: fontSize)]
+        let attributedString = NSMutableAttributedString(string: "\(boldString) \(string)")
+        let range = NSRange(location: 0, length: boldString.characters.count)
         attributedString.addAttributes(boldAttribute, range: range)
         
         let label: UILabel = UILabel(frame: CGRect.zero)
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 13)
+        label.font = UIFont.systemFont(ofSize: fontSize)
         label.numberOfLines = 0
         label.textColor = UIColor.darkGray()
         label.lineBreakMode = .byWordWrapping
-        
-        label.attributedText = attributedString
-        
-        return label
-    }
-    
-    
-    func createViewsLabel(with views: String) -> UILabel {
-        
-        let boldAttribute = [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 13)]
-        
-        let attributedString = NSMutableAttributedString(string: "Views: \(views)")
-        let range = NSRange(location: 0, length: 6)
-        
-        attributedString.addAttributes(boldAttribute, range: range)
-        
-        let label: UILabel = UILabel(frame: CGRect.zero)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 13)
-        label.numberOfLines = 0
-        label.textColor = UIColor.darkGray()
-        label.lineBreakMode = .byWordWrapping
-        
         label.attributedText = attributedString
         
         return label
